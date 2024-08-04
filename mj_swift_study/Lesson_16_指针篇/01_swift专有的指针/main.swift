a835e762b2cdce6fe4b02ed8b1c2113f88189d3e
whj <wanghaijun.425@163.com>
2024-08-05 02:56:00 +0800

    add:swift-指针篇项目创建.

---
+++ mj_swift_study/Lesson_16_指针篇/01_swift专有的指针/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  01_swift专有的指针
+//
+//  Created by whj on 2024/8/5.
+//
+/**
+ swift中有专门的指针
+ */
+
+import Foundation
+
+var age = 10
+
+func test1(_ ptr: UnsafeMutablePointer<Int>) {
+
+}
+
