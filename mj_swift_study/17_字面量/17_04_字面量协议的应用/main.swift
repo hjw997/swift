7a77539013254c21afc3fb3d412ff582b558ee35
whj <wanghaijun.425@163.com>
2024-08-09 06:25:35 +0800

    add:字面量协议的应用案例2.--代码格式化.

--- mj_swift_study/17_字面量/17_04_字面量协议的应用/main.swift
+++ mj_swift_study/17_字面量/17_04_字面量协议的应用/main.swift
@@ -18,28 +18,36 @@ struct Point {
 
 /// 实现字面量协议 数组 和 字典构造的
 
-extension Point: ExpressibleByArrayLiteral , ExpressibleByDictionaryLiteral {
+extension Point: ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral {
     /// 可变参数 ...
-    init(arrayLiteral elements: Double...){
-        guard elements.count > 0 else { return }
+    init(arrayLiteral elements: Double...) {
+        guard elements.count > 0 else {
+            return
+        }
         self.x = elements[0]
-        guard elements.count > 1 else { return }
+        guard elements.count > 1 else {
+            return
+        }
         self.y = elements[0]
     }
 
     /// 字典的可变参数: (String, Double)...
     init(dictionaryLiteral elements: (String, Double)...) {
-        for(key,value) in elements {
-            if key == "x" { self.x = value}
-            else if key == "y" {self.y = value}
+        for (key, value) in elements {
+            if key == "x" {
+                self.x = value
+            } else if key == "y" {
+                self.y = value
+            }
         }
     }
 }
+
 /// 如果这里不写类型 会被编辑器 当做是 Double 的数组类型
-var p:Point = [10.5,20.5]
+var p: Point = [10.5, 20.5]
 print(p)
 
-p = ["x":100.5,"y":0.8]
+p = ["x": 100.5, "y": 0.8]
 print(p)
 
 /**
