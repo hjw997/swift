3b3a268894df8a763d82c8667aac7390ed1ddf45
whj <wanghaijun.425@163.com>
2024-08-15 12:32:21 +0800

    add:一个OC对象自定义类的对象的内存窥探.

---
+++ mj_swift_study/01_oc对象本质/02_对象内存窥探技巧/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  02_对象内存窥探技巧
+//
+//  Created by whj on 2024/8/14.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Student : NSObject {
+@public
+    int _no;
+    int _age;
+}
+@end
+
+@implementation Student
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+    }
+    return 0;
+}
