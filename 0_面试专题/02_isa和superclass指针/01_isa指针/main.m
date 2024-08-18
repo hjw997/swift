bd7d81958e95539624c6a35133746a8dc78a913c
whj <wanghaijun.425@163.com>
2024-08-18 09:56:56 +0800

    add:面试专题

---
+++ 0_面试专题/02_isa和superclass指针/01_isa指针/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01_isa指针
+//
+//  Created by whj on 2024/8/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
